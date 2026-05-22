04524c0e1bd19c719f9b233e9a778f28721aa8b4
Hiromu Akiyama <akiyama@awa.localdomain>
2026-05-22 09:28:40 +0900

    モータ速度調整ができた

---
+++ moter_pwm/src/main.cpp
@@ -0,0 +1,50 @@
+#include <Arduino.h>
+
+const int RUN_SWITCH_PIN = 22; // HIGHのときドライバRUN
+// 10_CW 01_CCW 00_Brake
+const int ROTATE_PIN_1 = 18;
+const int ROTATE_PIN_2 = 19;
+const int PWM_PIN = 12; // 速度制御(PWM)
+
+// PWM関連
+const int ledcChannel = 0;
+const int freq = 20000;
+const int bit = 8;
+
+int duty = 1;
+bool flag = false;
+int count = 0;
+
+void setup() {
+  Serial.begin(115200); // シリアル通信開始
+
+  // PinをOUTPUTに設定
+  pinMode(RUN_SWITCH_PIN, OUTPUT);
+  pinMode(ROTATE_PIN_1, OUTPUT);
+  pinMode(ROTATE_PIN_2, OUTPUT);
+
+  // PinをHIGHにする
+  digitalWrite(RUN_SWITCH_PIN, HIGH);
+  digitalWrite(ROTATE_PIN_1, HIGH);
+  digitalWrite(ROTATE_PIN_2, LOW);
+
+  // PWMのピンを設定して割り当て
+  ledcSetup(ledcChannel, freq, bit);
+  ledcAttachPin(PWM_PIN, ledcChannel);
+}
+
+void loop() {
+  if (count > 5) {
+    digitalWrite(ROTATE_PIN_1, LOW);
+    digitalWrite(ROTATE_PIN_2, HIGH);
+  }
+  if (count > 10) {
+    digitalWrite(ROTATE_PIN_2, LOW);
+  }
+
+  ledcWrite(ledcChannel, 50);
+  delay(100);
+  ledcWrite(ledcChannel, 0);
+  delay(1000);
+  count++;
+}
